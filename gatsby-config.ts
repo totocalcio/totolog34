9261fc27e1ccf55457fdb5577f89301336b9f44d
totocalcio <totohyper@yahoo.co.jp>
2022-07-24 16:41:07 +0900

    gatsby-plugin-sitemap install

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -21,6 +21,7 @@ const config: GatsbyConfig = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-transformer-remark`,
       options: {
