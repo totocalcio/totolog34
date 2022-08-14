c30efb12a6ffdfda586574efbe18391aa7017e7c
totocalcio <totohyper@yahoo.co.jp>
2022-08-14 16:24:24 +0900

    404ページ修正

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'gatsby'
 import * as React from 'react'
 import { Layout } from '../components/layout'
 import { Seo } from '../components/seo'
@@ -6,6 +7,9 @@ const NotFoundPage: React.FC = () => {
   return (
     <Layout>
       <Seo title="404: Not Found" />
+      <h1>404</h1>
+      <p>ページが見つかりませんでした</p>
+      <Link to="/">TOPへ戻る</Link>
     </Layout>
   )
 }
