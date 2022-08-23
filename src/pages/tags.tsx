2ba3f14d4bd646ba74adedf6f60fd3eab8e72566
totocalcio <totohyper@yahoo.co.jp>
2022-08-23 09:11:33 +0900

    タグ一覧ページの見出し修正

--- src/pages/tags.tsx
+++ src/pages/tags.tsx
@@ -3,11 +3,20 @@ import { graphql, PageProps } from 'gatsby'
 import { Card } from '../components/card'
 import { Layout } from '../components/layout'
 import { Seo } from '../components/seo'
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
+import styled from 'styled-components'
 
 type Tag = {
   tag?: string
 }
+
+const StyledH1 = styled(Typography)`
+  &.MuiTypography-h1{
+    font-size: 2rem;
+    margin-top: 2rem;
+    margin-inline: 1rem;
+  }
+`
 
 const TagsPage: React.FC<PageProps<Queries.TagsPageQuery>> = ({
   data,
@@ -19,21 +28,21 @@ const TagsPage: React.FC<PageProps<Queries.TagsPageQuery>> = ({
   const { tag }: Tag = pageContext
 
   if (posts.length === 0) {
-    const tagHeader = `タグ: "${tag}" (0記事)`
+    const tagHeader = `"${tag}" (0記事)`
     return (
       <Layout>
-        <h1>{tagHeader}</h1>
+        <StyledH1 variant='h1'>{tagHeader}</StyledH1>
         <Seo title={tagHeader} />
         <p>該当するタグの投稿記事がありません。</p>
       </Layout>
     )
   }
 
-  const tagHeader = `タグ: "${tag}" (${totalCount}記事)`
+  const tagHeader = `"${tag}" (${totalCount}記事)`
 
   return (
     <Layout>
-      <h1>{tagHeader || 'Tag'}</h1>
+      <StyledH1 variant='h1'>{tagHeader || 'Tag'}</StyledH1>
       <Seo title={tagHeader || 'Tag'} />
       <Box
         sx={{
