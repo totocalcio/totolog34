7953448d12400baf322d98d809f11da6594f0f96
totocalcio <totohyper@yahoo.co.jp>
2024-12-30 17:21:57 +0900

    remove unused blog middleware

--- src/middleware/blog.ts
+++
@@ -1,5 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-  const split = to.path.split("/");
-  const path = split.at(-1) === "" ? `/${split.at(-2)}/` : `/${split.at(-1)}/`;
-  return { path };
-});
