277ad84a8a80a7a0e6bc6bec1d26a92fffaeb8aa
totocalcio <totohyper@yahoo.co.jp>
2024-12-30 14:45:41 +0900

    fix: update script and improve file path handling in content plugin

--- src/server/plugins/content.ts
+++ src/server/plugins/content.ts
@@ -1,7 +1,7 @@
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook("content:file:afterParse", (file: any) => {
     const prefix = "/blog";
-    if (file._path.startsWith(prefix)) {
+    if (file._path && file._path.startsWith(prefix)) {
       file._original_dir = prefix;
       file._path = file._path.replace(prefix, "");
     }
